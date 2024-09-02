4a2e91d50d55f73fabb54ae3be528bd315cc7a43
SASA-cloud <949804347@qq.com>
2024-09-02 01:16:01 -0700

    ok

--- examples/DRA/model_inversion_inverse_model_attack.py
+++ examples/DRA/model_inversion_inverse_model_attack.py
@@ -1,15 +1,11 @@
 '''
 Author: Ruijun Deng
-Date: 2024-04-16 12:25:20
-LastEditTime: 2024-04-16 14:15:05
+Date: 2024-08-24 00:41:30
+LastEditTime: 2024-09-02 01:15:35
 LastEditors: Ruijun Deng
-FilePath: /PP-Split/examples/model_inversion_inverse_model_attack.py
+FilePath: /PP-Split/examples/DRA/model_inversion_inverse_model_attack.py
 Description: 
 '''
-# %%
-# 这个notebook 介绍了 如何对split learning 发起 inverse-model attack攻击
-
-# %%
 # 导包
 import sys
 sys.path.append('/home/dengruijun/data/FinTech/PP-Split/')
@@ -20,69 +16,89 @@ import os
 
 # 防护措施
 from ppsplit.defense.noise import Noise
-noise_scale = 0
+
+# 模型、数据集获取
+from target_model.task_select import get_dataloader_and_model, get_dataloader,get_models
+
+# nohup python -u model_inversion_inverse_model_attack.py > ../../results/inverse-model-results-20240414//VGG5/InverseModelAttack/layer6.log 2>&1 &
+# 超参数
+args = {
+        'device':torch.device("cuda:1" if torch.cuda.is_available() else "cpu"),
+        # 'device':torch.device("cpu"),
+        'dataset':'CIFAR10',
+        # 'dataset':'bank',
+        # 'dataset':'credit',
+        # 'dataset':'purchase',
+        # 'dataset':'Iris',
+        # 'model': 'ResNet18',
+        'model': 'VGG5',
+        'result_dir': 'inverse-model-results-20240414/',
+        'oneData_bs': 1,
+        'test_bs': 1,
+        'train_bs': 32,
+        'noise_scale':0, # 防护措施
+        'split_layer': 6,
+        'test_num': 'InverseModelAttack'
+        # 'num_pairs': 10000, # RepE # 这个要另外准备
+        }
+
+print(args['device'])
+print(args)
+
+# 获取模型和数据集
+# msg = get_dataloader_and_model(**args)
+
+model_msg = get_models(args)
+
+# one_data_loader,trainloader,testloader = model_msg['one_data_loader'],model_msg['trainloader'], model_msg['testloader']
+client_net,decoder_net = model_msg['client_net'],model_msg['decoder_net']
+decoder_route = model_msg['decoder_route']
+image_deprocess = model_msg['image_deprocess']
+
+results_dir = model_msg['results_dir']
+inverse_dir = results_dir + 'layer'+str(args['split_layer'])+'/'
+data_type = 1 if args['dataset'] == 'CIFAR10' else 0
+
+print('results_dir:',results_dir)
+print('inverse_dir:',inverse_dir)
+print('decoder_route:',decoder_route)
 
 
-
-# %% [markdown]·
-# # Purchase100 数据集 （表格数据多分类）
-
-# %%
-# 导包和超参数设置
-from target_model.data_preprocessing.preprocess_purchase import preprocess_purchase,tabinfo_purchase
-
-from target_model.models.PurchaseNet import PurchaseClassifier1,PurchaseDecoder1,purchase_cfg
-from target_model.models.splitnn_utils import split_weights_client
-
-test_num = 1 # 测试编号（对应结果文件夹名称）
-split_layer = 3 # 模型切割点 （split point）在该层之前的层（含），作为client的模型，之后的层作为server的模型
-
-# 重要路径设置
-unit_net_route = '/home/dengruijun/data/FinTech/PP-Split/results/trained_models/Purchase100/Purchase_bestmodel_param.pth'
-results_dir = f'../results/inverse-model-results-20240414/Purchase/{test_num}/'
-inverse_dir = results_dir + 'layer'+str(split_layer)+'/'
-decoder_net_route = results_dir + f'Decoder-layer{split_layer}.pth' # 攻击的decoder net存储位置
-
-
-# %%
-# 准备target model的 client net（对模型进行切割）
-create_dir(results_dir)
-create_dir(inverse_dir)
-
-client_net = PurchaseClassifier1(layer=split_layer,noise_scale=noise_scale)
-pweights = torch.load(unit_net_route)
-if split_layer < len(purchase_cfg):
-    pweights = split_weights_client(pweights,client_net.state_dict())
-client_net.load_state_dict(pweights)
-
-
-
-# %%
 # 准备inverse_model attack使用到的东西
 # 创建Inverse Model Attack对象
-im_attack = InverseModelAttack(decoder_route=decoder_net_route,data_type=0,inverse_dir=inverse_dir)
+im_attack = InverseModelAttack(decoder_route=decoder_route,data_type=data_type,inverse_dir=inverse_dir)
 
 # 加载decoder模型
-if os.path.isfile(decoder_net_route): # 如果已经训练好了
-    print("=> loading decoder model '{}'".format(decoder_net_route))
-    decoder_net = torch.load(decoder_net_route)
-else: # 如果没有
-    print("train decoder model...")
-    decoder_net = PurchaseDecoder1(layer=split_layer)
+if not os.path.isfile(decoder_route): # 如果没有训练decoder
     # 训练decoder
-    trainloader,testloader = preprocess_purchase(batch_size=32)
-
+    args['train_bs']=32
+    args['test_bs']=32
+    msg_data = get_dataloader(args)
+    # trainloader,testloader = get_cifar10_normalize(batch_size=32)
     decoder_net= im_attack.train_decoder(client_net=client_net,decoder_net=decoder_net,
-                            train_loader=trainloader,test_loader=testloader,
+                            train_loader=msg_data['trainloader'],test_loader=msg_data['testloader'],
                             epochs=20)
+else:
+    print("Load decoder model from:",decoder_route)
 
-# %%
-# 实现攻击,恢复testloader中所有表格数据行
-trainloader,testloader = preprocess_purchase(batch_size=1)
+print(decoder_net)
+
+
+# 实现攻击,恢复testloader中所有图片
+# trainloader,testloader = get_cifar10_normalize(batch_size=1)
+args['train_bs']=1
+args['test_bs']=1
+msg_data = get_dataloader(args)
 
 im_attack.inverse(client_net=client_net,decoder_net=decoder_net,
-                  train_loader=trainloader,test_loader=testloader,
-                  save_fake=True,tab=tabinfo_purchase)
+                  train_loader=msg_data['trainloader'],test_loader=msg_data['testloader'],
+                  deprocess=image_deprocess,
+                  save_fake=True,
+                  tab=msg_data['tabinfo'])
 
 
-# [1] 21830
+
+
+
+
+
--- target_model/models/ResNet.py
+++ target_model/models/ResNet.py
@@ -1,3 +1,7 @@
+'''
+来源于 nips23 fisher的团队 改进的resnet
+'''
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
